324d42344ae72c6add78ce8f8d5626e702d5d92b
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-26 13:58:38 -0800

    commit tree 9a5909333e0682c6c3a2edd33114536bab7f17d4

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+const titleCased = function () {
+  return tutorials.map(iterateString)
+}
+
+
+/* const titleCased = () => {
   return tutorials
+} */
+
+//the below works to iterate through each tutorial and upper case first letter of each word
+//const titleCased = tutorials.map(iterateString);
+
+const string1 = 'what does the this keyword mean?';
+
+/* //split sentence string into array of words
+const string1Arr = string1.split(' ');
+
+//caplitalize first letter of a string
+function capitalizeFirst(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+//capitalize earch letter in sentence and return a sentence string
+const upperString1 = string1Arr.map(capitalizeFirst).join(' ');
+ */
+
+
+function iterateString(sentence) { 
+  const sentenceArr = sentence.split(' ');
+  const upperSentenceArr = sentenceArr.map(function (str) {
+    return str[0].toUpperCase() + str.slice(1);
+  })
+  return upperSentenceArr.join(' ');
 }
